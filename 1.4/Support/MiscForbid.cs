569de1d0ad32238f939cc7fa6b7345933a093a89
The Juggernaut <isaac@unl.edu>
2022-10-30 14:26:14 -0500

    forbid cleanup

--- 1.4/Support/MiscForbid.cs
+++ 1.4/Support/MiscForbid.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using Verse;
 
 namespace OgreStack.Support
@@ -7,9 +8,6 @@ namespace OgreStack.Support
 
 	internal class MiscForbid : ModDefinition
 	{
-		// Camping Stuff: 1523058989
-		private static readonly Regex RX_TENT = new Regex(@"^DeployableTent", RegexOptions.IgnoreCase);
-
 		internal MiscForbid() { }
 
 		//=====================================================================================================\\
@@ -23,18 +21,25 @@ namespace OgreStack.Support
 
 		internal override bool IsNonStackable(ThingDef thing)
 		{
-			// Camping Stuff : 1523058989
-			// Mods that alter stack size(OgreStack, Stack XXL, etc) - tents(DeployableTent, DeployableTentMedium, DeployableTentBig, 
-			// and DeployableTentLong) must have a stack size of 1.Tents in stacks(greater than one) will 
-			// not deploy and instead will disappear from the stack
+			HashSet<string> defsToBan = new HashSet<string>() {
+				// Camping Stuff : 1523058989
+				// Mods that alter stack size(OgreStack, Stack XXL, etc) - tents(DeployableTent, DeployableTentMedium, DeployableTentBig, 
+				// and DeployableTentLong) must have a stack size of 1.Tents in stacks(greater than one) will 
+				// not deploy and instead will disappear from the stack
+				"DeployableTent",
+				"DeployableTentMedium",
+				"DeployableTentBig",
+				"DeployableTentLong",
 
-			if (RX_TENT.IsMatch(thing.defName))
+				// DLC: BioTech
+				"Genepack",
+				"HumanEmbryo",
+				"Xenogerm"
+			};
+
+			if (defsToBan.Contains(thing.defName))
 				return true;
 
-
-			// Biotech DLC
-			if (string.Compare(thing.defName, "Genepack", true) == 0) { return false; }
-			if (string.Compare(thing.defName, "HumanEmbryo", true) == 0) { return false; }
 
 			return false;
 		}
